5087375288ee34b8e27c62fbce4bd42b5b1f54b1
Pedro Lucas de Almeida <sr.pedrodealmeida@gmail.com>
2019-09-04 23:33:01 -0300

    Last commit today

--- api-node-postgres/app.js
+++ api-node-postgres/app.js
@@ -1,7 +1,12 @@
-const port = 3000;
-
+const port = 5000;
+const path = require('path');
 const app = require('./config/express.js')();
+
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'ejs');
+app.set('css', path.join(__dirname, 'aquelamerdanaofuncioan'))
 
 app.listen(port, function() {
     console.log('App rodando na porta ' + port + ' .');
+    
 });
